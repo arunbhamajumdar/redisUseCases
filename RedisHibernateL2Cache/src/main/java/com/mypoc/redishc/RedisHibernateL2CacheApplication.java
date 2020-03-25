daf390ef82d0a302252b3973900d4ff0bdc3a8dc
cw22is2 <cw22is2@FEPVFWIN10-1246.fepoc.com>
2020-03-25 15:40:31 -0400

    Initial commit.

---
+++ RedisHibernateL2Cache/src/main/java/com/mypoc/redishc/RedisHibernateL2CacheApplication.java
@@ -0,0 +1,13 @@
+package com.mypoc.redishc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisHibernateL2CacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisHibernateL2CacheApplication.class, args);
+	}
+
+}
---
+++ RedisHibernateL2Cache/src/test/java/com/mypoc/redishc/RedisHibernateL2CacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypoc.redishc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisHibernateL2CacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
