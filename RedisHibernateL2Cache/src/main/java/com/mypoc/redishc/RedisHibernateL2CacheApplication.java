8134b889e41b526509a9bc6b1aa290ff5fe3adb5
arunbhamajumdar <arunbhamajumdar@gmail.com>
2020-03-29 17:39:03 -0400

    Redisson updates

--- RedisHibernateL2Cache/src/main/java/com/mypoc/redishc/RedisHibernateL2CacheApplication.java
+++ RedisHibernateL2Cache/src/main/java/com/mypoc/redishc/RedisHibernateL2CacheApplication.java
@@ -2,8 +2,10 @@ package com.mypoc.redishc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class RedisHibernateL2CacheApplication {
 
 	public static void main(String[] args) {
